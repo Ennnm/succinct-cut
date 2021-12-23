068dd259a8cb0fa92633bdc148ebd70714c36742
ennnm <jiaen.1sc4@gmail.com>
2021-12-23 14:53:29 +0800

    sync-ed up ibmwatsons

--- pages/index.js
+++ pages/index.js
@@ -12,9 +12,9 @@ import { Loader } from '../components/Loader';
 import { ffmpegContext, UserContext } from '../lib/context';
 // import { transcript } from '../refTranscriptData/transcript_en.js';
 // import { transcript } from '../refTranscriptData/transcript_2_flac_narrowband';
-import { transcript } from '../refTranscriptData/cxTranscripts5min';
+// import { transcript } from '../refTranscriptData/cxTranscripts5min';
 // import { transcript } from '../refTranscriptData/cxTranscripts2min';
-// import { transcript } from '../refTranscriptData/cxTranscripts1min';
+import { transcript } from '../refTranscriptData/cxTranscripts1min';
 // ============FIREBASE=============
 import { doc, deleteDoc, onSnapshot } from 'firebase/firestore';
 //import needed to get firebase initiated
@@ -31,7 +31,7 @@ import {
   Tooltip,
   Spinner,
   useToast,
-  Container
+  Container,
 } from '@chakra-ui/react';
 
 export default function Home() {
@@ -43,7 +43,8 @@ export default function Home() {
   const [audioUuid, setAudioUuid] = useState();
   const [cleanedClip, setCleanedClip] = useState(false);
   //TODO remove trasncript
-  const [transcription, setTranscription] = useState(transcript.result);
+  const [transcription, setTranscription] = useState();
+  // const [transcription, setTranscription] = useState(transcript.result);
   const ffmpegRatio = useRef(0);
   const [processStage, setProcessStage] = useState([]);
   const [progress, setProgress] = useState(0);
@@ -56,20 +57,20 @@ export default function Home() {
 
   const NORMS = {};
 
-  NORMS[stage.LOADING_VIDEO] = 5
-  NORMS[stage.EXTRACTING_AUDIO] = 5
-  NORMS[stage.EXTRACTED_AUDIO] = 5
-  NORMS[stage.UPLOADING_AUDIO] =5
-  NORMS[stage.ANALYSING_AUDIO] = 5
-  NORMS[stage.ANALYSED_AUDIO] = 25
-  NORMS[stage.PROCESSING_VIDEO] = 5
-  NORMS[stage.CLEANING_TRANSCRIPT] = 5
-  NORMS[stage.CUTTING_VIDEO] = 5
-  NORMS[stage.SPEEDING_UP_PAUSES] = 16
-  NORMS[stage.PREPARING_TO_STITCH] = 4
-  NORMS[stage.STITCHING_VIDEO] = 5
-  NORMS[stage.CLEARING_MEMORY] =9
-  NORMS[stage.COMPLETED] = 1
+  NORMS[stage.LOADING_VIDEO] = 5;
+  NORMS[stage.EXTRACTING_AUDIO] = 5;
+  NORMS[stage.EXTRACTED_AUDIO] = 5;
+  NORMS[stage.UPLOADING_AUDIO] = 5;
+  NORMS[stage.ANALYSING_AUDIO] = 5;
+  NORMS[stage.ANALYSED_AUDIO] = 25;
+  NORMS[stage.PROCESSING_VIDEO] = 5;
+  NORMS[stage.CLEANING_TRANSCRIPT] = 5;
+  NORMS[stage.CUTTING_VIDEO] = 5;
+  NORMS[stage.SPEEDING_UP_PAUSES] = 16;
+  NORMS[stage.PREPARING_TO_STITCH] = 4;
+  NORMS[stage.STITCHING_VIDEO] = 5;
+  NORMS[stage.CLEARING_MEMORY] = 9;
+  NORMS[stage.COMPLETED] = 1;
 
   let user = auth.currentUser;
 
@@ -87,7 +88,7 @@ export default function Home() {
     setTimeTaken(timeTaken);
     setProcessStage(processStage);
 
-    setProgress(progress => progress + +NORMS[stage]);
+    setProgress((progress) => progress + +NORMS[stage]);
   };
 
   const load = async () => {
@@ -129,7 +130,13 @@ export default function Home() {
             setTranscription(result);
             timeStampAtStage(stage.ANALYSED_AUDIO);
             const { results } = result;
-            const lastResultTimeStamps = results[results.length - 1].timeStamps;
+            console.log('result', result);
+            console.log('results', results);
+            const lastResult = results[results.length-1]
+            console.log('lastResult', lastResult);
+            console.log('lastResult.alternatives[0]');
+            const lastResultTimeStamps = lastResult.alternatives[0].timestamps;
+            console.log('lastResultTimeStamps', lastResultTimeStamps);
             const lastTime =
               lastResultTimeStamps[lastResultTimeStamps.length - 1][2];
             setTranscriptDuration(lastTime);
@@ -170,15 +177,14 @@ export default function Home() {
     }
     return durations;
   };
-  const [ transcriptList, setTranscriptList ] = useState();
-  const [ optimizedList, setOptimizedList ] = useState( false );
-  const [ remainingPercentage, setRemainingPercentage ] = useState(100);
-  const [ transcriptDuration, setTranscriptDuration] = useState();
+  const [transcriptList, setTranscriptList] = useState();
+  const [optimizedList, setOptimizedList] = useState(false);
+  const [remainingPercentage, setRemainingPercentage] = useState(100);
+  const [transcriptDuration, setTranscriptDuration] = useState();
   useEffect(() => {
     console.log('mergedTranscript', mergedTranscript);
     if (!!mergedTranscript) {
-      const tduration =
-        mergedTranscript[mergedTranscript.length - 1].endTime;
+      const tduration = mergedTranscript[mergedTranscript.length - 1].endTime;
       setTranscriptDuration(tduration);
       const colorMap = {
         WORD: 'green',
@@ -259,7 +265,9 @@ export default function Home() {
             duration = 0.8;
           }
           percentage = (duration / tduration) * 100;
-          setRemainingPercentage(remainingPercentage => remainingPercentage - percentage);
+          setRemainingPercentage(
+            (remainingPercentage) => remainingPercentage - percentage
+          );
 
           tType = '[ BREAK ]';
 
@@ -299,7 +307,9 @@ export default function Home() {
             break;
         }
 
-        setRemainingPercentage(remainingPercentage => remainingPercentage - percentage);
+        setRemainingPercentage(
+          (remainingPercentage) => remainingPercentage - percentage
+        );
         memoPrevEnd2 = t.endTime;
 
         return (
@@ -348,22 +358,22 @@ export default function Home() {
                 {message && <div>{message}</div>}
               </div>
               <div className={styles.card}>
-                { video && (
-                  (processStage.length > 1 && cleanedClip) ?
+                {video &&
+                  (processStage.length > 1 && cleanedClip ? (
                     <video
                       controls
                       className={styles.video}
                       src={cleanedClip}
-                    ></video> :
+                    ></video>
+                  ) : (
                     <Spinner
-                      thickness='4px'
-                      speed='0.65s'
-                      emptyColor='gray.200'
-                      color='blue.500'
-                      size='xl'
+                      thickness="4px"
+                      speed="0.65s"
+                      emptyColor="gray.200"
+                      color="blue.500"
+                      size="xl"
                     />
-                  )
-                }
+                  ))}
               </div>
             </div>
             <div className={styles.grid}>
@@ -374,27 +384,29 @@ export default function Home() {
               </div>
               <div className={styles.minicard}>
                 <HStack width="100vw" border="0.5px solid" spacing={0}>
-                  { optimizedList ? ( 
+                  {optimizedList ? (
                     <>
-                      {optimizedList}   
+                      {optimizedList}
                       <Tooltip
-                          hasArrow
-                          label={`Potential Time Savings = ${Math.abs(Math.round(
-                            remainingPercentage
-                          ))}% | ${Math.abs(Math.round(
+                        hasArrow
+                        label={`Potential Time Savings = ${Math.abs(
+                          Math.round(remainingPercentage)
+                        )}% | ${Math.abs(
+                          Math.round(
                             (remainingPercentage / 100) * transcriptDuration
-                          ))}s`}
+                          )
+                        )}s`}
+                      >
+                        <Flex
+                          width={`${Math.abs(remainingPercentage)}%`}
+                          height="20px"
+                          bgColor="white"
                         >
-                          <Flex
-                            width={`${Math.abs(remainingPercentage)}%`}
-                            height="20px"
-                            bgColor="white"
-                          >
-                            {' '}
-                          </Flex>
-                        </Tooltip>
+                          {' '}
+                        </Flex>
+                      </Tooltip>
                     </>
-                    ) : null }
+                  ) : null}
                 </HStack>
               </div>
             </div>
@@ -425,8 +437,8 @@ export default function Home() {
                 Browse ...
               </Button>
 
-              <Button variant="ghost"
-              
+              <Button
+                variant="ghost"
                 onClick={() => {
                   extractAudioClip(
                     ffmpeg,
@@ -439,13 +451,13 @@ export default function Home() {
                   setTimeTaken([]);
                   setAudioAnalysisBegan(true);
                 }}
-                isDisabled={!video || processStage.length > 0 }
+                isDisabled={!video || processStage.length > 0}
               >
                 Analyse Video
               </Button>
 
-              <Button variant="ghost"
-              
+              <Button
+                variant="ghost"
                 onClick={() => {
                   cleanClip(
                     transcription,
@@ -459,46 +471,53 @@ export default function Home() {
                     setTranscriptDuration
                   );
                 }}
-                isDisabled={!transcription || !video }
+                isDisabled={!transcription || !video}
               >
                 Clean Video
               </Button>
-              <Button variant="ghost"
-              
+              <Button
+                variant="ghost"
                 onClick={onDownloadClick}
                 isDisabled={!cleanedClip}
               >
                 Download
               </Button>
-                <a
-                  ref={downloadAnchor}
-                  href={cleanedClip}
-                  download={'result.mp4'}
-                  style={{ display: 'none' }}
-                >
-                  Click to download
-                </a>
+              <a
+                ref={downloadAnchor}
+                href={cleanedClip}
+                download={'result.mp4'}
+                style={{ display: 'none' }}
+              >
+                Click to download
+              </a>
             </div>
             <Flex direction="column" justify="center" align="center">
-              <Progress height="20px" w="90vw" hasStripe isAnimated value={progress}>
+              <Progress
+                height="20px"
+                w="90vw"
+                hasStripe
+                isAnimated
+                value={progress}
+              >
                 <ProgressLabel fontSize="1rem" color="black">
-                    { `${progress}%`}
+                  {`${progress}%`}
                 </ProgressLabel>
               </Progress>
-              <Container
-                textAlign="center"
-              >
-                {processStage.length > 0 &&
+              <Container textAlign="center">
+                {
+                  processStage.length > 0 &&
                     timeTaken.length > 0 &&
-                    `${processStage.at(-1)} | Time taken: ${
-                      ((timeTaken.at(-1) - timeTaken.at(0)) / 1000 / 60).toFixed(2)
-                    } min`
-                    // && processStage.map((stage, i) => (
-                    //   <span key={i}>
-                    //     {stage}: {calcTimeTakenPerStage()[i]}
-                    //   </span>
-                    // ))
-                  }
+                    `${processStage.at(-1)} | Time taken: ${(
+                      (timeTaken.at(-1) - timeTaken.at(0)) /
+                      1000 /
+                      60
+                    ).toFixed(2)} min`
+                  // && processStage.map((stage, i) => (
+                  //   <span key={i}>
+                  //     {stage}: {calcTimeTakenPerStage()[i]}
+                  //   </span>
+                  // ))
+                }
               </Container>
             </Flex>
             {transcription && <p>{JSON.stringify(transcription)}</p>}
